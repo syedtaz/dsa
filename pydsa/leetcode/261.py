4db1d4cc23be37aa6a906e2b1bcd9a0402ba81d4
Tazmilur Saad <ssaad@colgate.edu>
2023-12-31 02:03:24 -0500

    Some more graph leetcodes.

---
+++ pydsa/leetcode/261.py
@@ -0,0 +1,44 @@
+from typing import List
+from collections import defaultdict, deque
+
+
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        if len(edges) != n - 1:
+            return False
+
+        def construct(edges: List[List[int]]) -> dict[int, list[int]]:
+            g: dict[int, list[int]] = defaultdict(list)
+
+            for edge in edges:
+                fr = edge[0]
+                to = edge[1]
+                g[fr].append(to)
+                g[to].append(fr)
+
+            return g
+
+        graph = construct(edges)
+        seen: set[int] = set()
+
+        def dfs(node: int, parent: int):
+            if node in seen:
+                return True
+
+            seen.add(node)
+            for edge in graph[node]:
+                if edge == parent:
+                    continue
+                if edge in seen:
+                    return False
+                result = dfs(edge, node)
+                if not result:
+                    return False
+
+            return True
+
+        return dfs(0, -1) and len(seen) == n
+
+
+# s = Solution()
+# print(s.validTree(5, [[0,1],[0,2],[0,3],[1,4]]))
---
+++ pydsa/leetcode/277.py
@@ -0,0 +1,20 @@
+graph = [[1,0,1],[1,1,0],[0,1,1]]
+
+def knows(a: int, b: int) -> bool:
+    return graph[a][b] == 1
+
+class Solution:
+    def findCelebrity(self, n: int) -> int:
+        candidate = 0
+
+        for i in range(1, n):
+            if knows(candidate, i):
+                candidate = i
+
+        for j in range(n):
+            if j == candidate:
+                continue
+            if knows(candidate, j) or not knows(j, candidate):
+                return -1
+
+        return candidate
