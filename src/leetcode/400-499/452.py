97e591f355ad1410453aa2b77ba83c21a551b46a
Tazmilur Saad <ssaad@colgate.edu>
2024-03-19 01:18:06 -0400

    Some dailies

---
+++ src/leetcode/400-499/452.py
@@ -0,0 +1,25 @@
+from typing import List
+
+class Solution:
+    def findMinArrowShots(self, points: List[List[int]]) -> int:
+
+        points.sort()
+        i = 0
+        acc = 0
+
+        while i < len(points):
+            start, end = points[i]
+            j = i + 1
+
+            while j < len(points):
+                if points[j][0] > end:
+                    break
+
+                nstart, nend = points[j]
+                start, end = max(start, nstart), min(end, nend)
+                j = j + 1
+
+            acc += 1
+            i = j
+
+        return acc
