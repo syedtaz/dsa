8062a625ce507857376f8cdfe5706275ec820029
Tazmilur Saad <ssaad@colgate.edu>
2024-03-29 18:20:54 -0400

    Updated readme.

---
+++ src/leetcode/700-799/713.py
@@ -0,0 +1,11 @@
+from typing import List
+
+class Solution:
+    def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
+        acc = 1
+        prefs = [acc := acc * x for x in nums]
+        print(prefs)
+        return 0
+
+s = Solution()
+print(s.numSubarrayProductLessThanK(nums = [10,5,2,6], k = 100))
