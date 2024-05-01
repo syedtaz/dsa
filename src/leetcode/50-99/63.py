ca1fe7340975ca10b6fe6f64531fef5509ad05a1
Tazmilur Saad <ssaad@colgate.edu>
2024-04-30 21:44:26 -0400

    Comeback?

--- src/leetcode/50-99/63.py
+++ src/leetcode/50-99/63.py
@@ -1,4 +1,26 @@
 from typing import List
+from functools import cache
+
+point = tuple[int, int]
+
 
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        m, n = len(obstacleGrid), len(obstacleGrid[0])
+
+        @cache
+        def search(positition: point) -> int:
+            x, y = positition
+
+            if obstacleGrid[x][y] == 1:
+                return 0
+
+            if positition == (m - 1, n - 1):
+                return 1
+
+            right = search((x + 1, y)) if (0 <= x + 1 < m) else 0
+            down = search((x, y + 1)) if (0 <= y + 1 < n) else 0
+
+            return right + down
+
+        return search((0, 0))
